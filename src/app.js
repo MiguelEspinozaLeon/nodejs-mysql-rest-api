61f706a0455f18086f3de5a960b47c5902557c6b
miguel.espinozaleon <miguel.espinozaleon@gmail.com>
2023-04-03 12:20:31 -0700

    cors added

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from 'express'
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+var cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', indexRoutes);
 app.use('/api', employeesRoutes);
+app.use.
 
 app.use((req, res, next) => {
     res.status(404).json({
